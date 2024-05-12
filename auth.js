a86a9afc2d303c344428b5df3227848dd5a049d9
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-12 22:30:22 +0600

    chore: Update Google client credentials and remove unused code

--- auth.js
+++ auth.js
@@ -1,17 +1,16 @@
-
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const {
     handlers: { GET, POST },
-    // auth,
-    // signIn,
-    // signOut,
+    auth,
+    signIn,
+    signOut,
 } = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: "801748148316-9dkco504uv7tjlh078dmqmbqh411rfbv.apps.googleusercontent.com",
+            clientSecret: "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AUTH_SECRET=b310c60e5f230b7ac5386932507b963d74d35dab15367a09fcb513f22fbac1a2",
         })
     ],
 });
