a3becc9349e6380c62c83394e18dbbe58d043d1f
Tanmoy paul <84624966+tanmoypaul1005@users.noreply.github.com>
2024-05-15 08:09:57 +0600

    done

---
+++ app/api/product/route.js
@@ -0,0 +1,38 @@
+import { Product } from "@/models/Product";
+import { products_type } from "@/utils/const";
+import connectMongo from "@/utils/db";
+
+export const dynamic = 'force-dynamic' 
+
+
+export async function GET(request) {
+    try {
+        await connectMongo();
+        const searchParams = request.nextUrl.searchParams;
+        const query = searchParams.get("type");
+        let products = [];
+        if (products_type.includes(query)) {
+          products = await Product.find({ type: query });
+        }
+        else if (query === null) {
+          products = await Product.find({});
+        }
+        else {
+          return Response.json({ success: false, status: 400, message: "Invalid type", data: null });
+        }
+    
+        // products = await Promise.all(products.map(async (product) => {
+        //   const category = await Category.findOne({ _id: product.category });
+        //   if (category) {
+        //     product.category = category;
+        //   }
+        //   return product;
+        // }));
+        
+        return Response.json({ status: 200, success: true, data: products, message: "Product is Found" });
+      } catch (err) {
+        console.error(err);
+        return Response.json({ success: false, status: 500, message: "Internal Server Error", data: null });
+      }
+    
+}
---
+++ utils/const.js
@@ -0,0 +1,15 @@
+export const products_type_value = {
+    new_arrivals: "new_arrivals",
+    trending: "trending",
+    top_rated: "top_rated",
+    regular: "regular",
+    popular: "popular",
+  };
+  
+  export const products_type = [
+    products_type_value.new_arrivals,
+    products_type_value.trending,
+    products_type_value.top_rated,
+    products_type_value.regular,
+    products_type_value.popular,
+  ];
